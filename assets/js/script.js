64056398454a7a3b04b64621533e767abeb45c97
Mona Lisa <sam.roberson32@gmail.com>
2021-04-27 18:08:36 -0600

    added hide functionality to all quiz sets

--- assets/js/script.js
+++ assets/js/script.js
@@ -15,14 +15,42 @@ var startBtn = document.querySelector("#startBtn");
 var introSection = document.querySelector(".intro-section");
 var mainSection = document.querySelector(".main-section");
 var timer = document.querySelector("#timer");
+var choiceBtn = document.querySelectorAll(".choiceBtn");
+var saveBtn = document.querySelector("#saveBtn");
 var myClock;
+var highScore = JSON.parse(localStorage.getItem("highScore")) || [];
 startBtn.addEventListener("click", startGame);
-
+saveBtn.addEventListener("click", saveInitials);
 var secondsLeft = 75;
+
+function saveInitials() {
+  var initials = document.querySelector("#initials").value;
+  highScore.push({ initials: initials, highScore: secondsLeft });
+  localStorage.setItem("highScore", JSON.stringify(highScore));
+}
+
 function startGame() {
   introSection.classList.add("hide");
   mainSection.classList.remove("hide");
+
+  for (var i = 0; i < choiceBtn.length; i++) {
+    choiceBtn[i].addEventListener("click", nextQuestion);
+  }
   myClock = setInterval(startTimer, 1000);
+}
+
+function nextQuestion() {
+  var currentArticle = this.getAttribute("data-article");
+  var nextArticle = this.getAttribute("data-nextArticle");
+  document.querySelector("." + currentArticle).classList.add("hide");
+  if (nextArticle !== "end") {
+    document.querySelector("." + nextArticle).classList.remove("hide");
+  }
+
+  if (nextArticle === "end") {
+    clearInterval(myClock);
+    document.querySelector(".initials-section").classList.remove("hide");
+  }
 }
 
 function startTimer() {
